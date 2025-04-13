7a0736584430d9b631cce399fc175d0d9adfbb88
X1n9fU <minkyeong0244@gmail.com>
2025-04-14 05:07:39 +0900

    [refactor] Attributes를 dto로 관리하여 모듈화 및 UserLoginState 반환 로직 리팩토링  #3

--- src/main/java/dev/book/global/config/security/service/Oauth2AuthService.java
+++ src/main/java/dev/book/global/config/security/service/Oauth2AuthService.java
@@ -1,10 +1,11 @@
 package dev.book.global.config.security.service;
 
+import dev.book.global.config.security.dto.OAuth2Attributes;
 import dev.book.global.config.security.exception.CustomOAuth2Error;
 import dev.book.global.config.security.exception.UnValidatedProviderException;
 import dev.book.user.entity.UserEntity;
 import dev.book.user.enums.UserLoginState;
-import dev.book.user.service.UserService;
+import dev.book.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -13,17 +14,16 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 /**
- * OAuth2User에서 필요한 정보를 가져온다.
- * - kakao
- *   profile_nickname, profile_image, account_email
+ * OAuth2User에서 attributes를 통해 유저의 상태 파악 후 UserLoginState 반환
  */
 @Service
 @RequiredArgsConstructor
 public class Oauth2AuthService {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
 
     public UserLoginState getAttributes(Authentication authentication, HttpServletResponse response){
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
@@ -32,7 +32,8 @@ public class Oauth2AuthService {
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
         if (provider.equals("kakao")){
-            return getKakaoAttributes(attributes);
+            OAuth2Attributes kakaoAttributes = OAuth2Attributes.toKakao(attributes);
+            return getUserLoginState(kakaoAttributes);
         } else {
             CustomOAuth2Error oAuth2Error = new CustomOAuth2Error("Invalid_Provider", "지원되지 않는 공급자입니다.", null, 403);
             throw new UnValidatedProviderException(oAuth2Error);
@@ -40,26 +41,26 @@ public class Oauth2AuthService {
     }
 
     /**
-     * attributes에서 필요한 정보들을 반환한다.
-     * @param attributes
-     * @return DB에서 유저를 조회하여 존재한다면 LOGIN_SUCCESS, 존재하지 않으면 현재 kakao 정보들만 저장한 후에 PROFILE_INCOMPLETE
+     * DB에서 유저를 조회하여 존재한다면 LOGIN_SUCCESS
+     * nickname이 비어 있거나 존재하지 않으면 현재 kakao 정보들만 저장한 후에 PROFILE_INCOMPLETE
+     * @param OAuth2Attributes
+     * @return
      */
-    private UserLoginState getKakaoAttributes(Map<String, Object> attributes) {
-        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
-        Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");
-        String profileImageUrl = (String) profile.get("profile_image_url");
-        String nickname = (String) profile.get("nickname");
-        String email = (String) kakao_account.get("email");
-
-        if (userService.isUserExist(email)) return UserLoginState.LOGIN_SUCCESS;
+    private UserLoginState getUserLoginState(OAuth2Attributes OAuth2Attributes) {
+        Optional<UserEntity> user = userRepository.findByEmail(OAuth2Attributes.email());
+        if (user.isPresent()){
+            if (user.get().getNickname().isBlank())
+                return UserLoginState.PROFILE_INCOMPLETE;
+            else return UserLoginState.LOGIN_SUCCESS;
+        }
         else {
             UserEntity newUser = UserEntity.builder()
-                    .email(email)
-                    .name(nickname)
+                    .email(OAuth2Attributes.email())
+                    .name(OAuth2Attributes.nickname())
                     .nickname("")
-                    .profileImageUrl(profileImageUrl)
+                    .profileImageUrl(OAuth2Attributes.profileImageUrl())
                     .build();
-            userService.unComplete(newUser);
+            userRepository.save(newUser);
             return UserLoginState.PROFILE_INCOMPLETE;
         }
     }
