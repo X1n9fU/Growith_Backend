e7d6cf4a90b2f2694600ef3e699a365fd2c7ff6c
X1n9fU <minkyeong0244@gmail.com>
2025-04-14 03:11:04 +0900

    [feat] OAuth2 로그인 성공, 실패 시 반환 handler 구현 #3

---
+++ src/main/java/dev/book/global/config/security/handler/OAuth2FailureHandler.java
@@ -0,0 +1,24 @@
+package dev.book.global.config.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/*
+ OAuth2 로그인 실패 시
+ */
+@Component
+public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        getRedirectStrategy().sendRedirect(request, response, "/login");
+    }
+}
---
+++ src/main/java/dev/book/global/config/security/handler/OAuth2SuccessHandler.java
@@ -0,0 +1,38 @@
+package dev.book.global.config.security.handler;
+
+import dev.book.global.config.security.service.Oauth2AuthService;
+import dev.book.user.enums.UserLoginState;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/*
+OAuth2 로그인 성공 시
+ */
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final Oauth2AuthService oauth2AuthService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        //가져온 인증 정보를 통해 유저 존재 여부 확인 후 리다이렉션 진행
+        UserLoginState userLoginState = oauth2AuthService.getAttributes(authentication, response);
+
+        switch(userLoginState){ //todo 반환되는 화면 경로 정하기
+            case LOGIN_SUCCESS ->
+                getRedirectStrategy().sendRedirect(request, response, "/main");
+            case PROFILE_INCOMPLETE ->
+                getRedirectStrategy().sendRedirect(request, response, "/signup");
+        }
+    }
+}
