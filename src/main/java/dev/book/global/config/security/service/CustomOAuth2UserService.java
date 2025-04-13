d6ccdc27c676286a1d4a739baf07fc8072d809e9
X1n9fU <minkyeong0244@gmail.com>
2025-04-14 05:03:24 +0900

    [feat] DefaultOAuth2UserService을 상속하여 SecurityContext에 Authentication 등록 #3

---
+++ src/main/java/dev/book/global/config/security/service/CustomOAuth2UserService.java
@@ -0,0 +1,36 @@
+package dev.book.global.config.security.service;
+
+import dev.book.global.config.security.dto.CustomOAuth2User;
+import dev.book.global.config.security.dto.OAuth2Attributes;
+import dev.book.global.config.security.exception.CustomOAuth2Error;
+import dev.book.global.config.security.exception.UnValidatedProviderException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * OAuth2 로그인 결과를 CustomOAuth2User로 생성하여 SecurityContext에 등록한다.
+ */
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+
+        if (provider.equals("kakao")){
+            OAuth2Attributes kakaoAttributes = OAuth2Attributes.toKakao(attributes);
+            return new CustomOAuth2User(kakaoAttributes.email(), attributes);
+        } else {
+            CustomOAuth2Error oAuth2Error = new CustomOAuth2Error("Invalid_Provider", "지원되지 않는 공급자입니다.", null, 403);
+            throw new UnValidatedProviderException(oAuth2Error);
+        }
+    }
+
+}
