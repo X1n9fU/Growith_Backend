e16b939c0e6ae485cf90f47ae326425e39073f36
X1n9fU <minkyeong0244@gmail.com>
2025-04-16 02:41:29 +0900

    [refactor] ErrorCode, Exception 도메인 별로 설정 #7

--- src/main/java/dev/book/user/exception/DuplicateNicknameException.java
+++
@@ -1,7 +0,0 @@
-package dev.book.user.exception;
-
-public class DuplicateNicknameException extends RuntimeException {
-    public DuplicateNicknameException(String msg) {
-        super(msg);
-    }
-}
