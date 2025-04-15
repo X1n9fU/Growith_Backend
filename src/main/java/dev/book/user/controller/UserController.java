ee4920a339367ec5d5eea5e8a56e8661312c6752
X1n9fU <minkyeong0244@gmail.com>
2025-04-15 18:46:51 +0900

    [feat] 유저 탈퇴 시 토큰 쿠키 삭제 #7

--- src/main/java/dev/book/user/controller/UserController.java
+++ src/main/java/dev/book/user/controller/UserController.java
@@ -4,6 +4,8 @@ import dev.book.global.config.security.dto.CustomUserDetails;
 import dev.book.user.dto.request.UserProfileUpdateRequest;
 import dev.book.user.dto.response.UserProfileResponse;
 import dev.book.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -34,8 +36,10 @@ public class UserController {
     //todo 카테고리 변경
 
     @DeleteMapping
-    public ResponseEntity<?> deleteUser(@AuthenticationPrincipal CustomUserDetails userDetails){
-        userService.deleteUser(userDetails);
+    public ResponseEntity<?> deleteUser(
+            HttpServletRequest request, HttpServletResponse response,
+            @AuthenticationPrincipal CustomUserDetails userDetails){
+        userService.deleteUser(request, response, userDetails);
         return ResponseEntity.ok()
                 .build();
     }
--- src/main/java/dev/book/user/service/UserService.java
+++ src/main/java/dev/book/user/service/UserService.java
@@ -1,11 +1,14 @@
 package dev.book.user.service;
 
 import dev.book.global.config.security.dto.CustomUserDetails;
+import dev.book.global.config.security.jwt.JwtUtil;
 import dev.book.global.config.security.service.AuthService;
 import dev.book.user.dto.request.UserProfileUpdateRequest;
 import dev.book.user.dto.response.UserProfileResponse;
 import dev.book.user.entity.UserEntity;
 import dev.book.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final AuthService authService;
+    private final JwtUtil jwtUtil;
 
     public UserProfileResponse getUserProfile(CustomUserDetails userDetails) {
         return UserProfileResponse.fromEntity(userDetails.user());
@@ -34,9 +38,16 @@ public class UserService {
         return UserProfileResponse.fromEntity(user);
     }
 
+    /**
+     * Cookie에 설정된 AcessToken, RefreshToken 삭제
+     * DB에서 유저 완전히 삭제
+     * @param request
+     * @param response
+     * @param userDetails
+     */
     @Transactional
-    public void deleteUser(CustomUserDetails userDetails) {
+    public void deleteUser(HttpServletRequest request, HttpServletResponse response, CustomUserDetails userDetails) {
+        jwtUtil.deleteAccessTokenAndRefreshToken(request, response);
         userRepository.delete(userDetails.user());
-        //todo 연관된 entity 대하여 삭제 처리
     }
 }
