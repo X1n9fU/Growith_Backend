9a920ad03a85f6c14b1f16bea3c04223de32338d
BHC-Chicken <tij05078@gmail.com>
2025-04-16 22:34:28 +0900

    [refactor] 소비 항목 못찾았을 경우 예외 반환
    - DB에서 소비 항목들 못 찾았을 경우 예외 반환하도록 변경

--- src/main/java/dev/book/accountbook/service/AccountBookService.java
+++ src/main/java/dev/book/accountbook/service/AccountBookService.java
@@ -31,6 +31,10 @@ public class AccountBookService {
     public List<AccountBookSpendResponse> getSpendList(Long userId) {
         List<AccountBook> accountBooks = accountBookRepository.findAllByUserIdAndType(userId, CategoryType.SPEND);
 
+        if (accountBooks.isEmpty()) {
+            throw new AccountBookErrorException(AccountBookErrorCode.NOT_FOUND_SPEND);
+        }
+
         return accountBooks.stream()
                 .map(AccountBookSpendResponse::from)
                 .toList();
@@ -67,6 +71,10 @@ public class AccountBookService {
 
     public List<AccountBookIncomeResponse> getIncomeList(Long userId) {
         List<AccountBook> accountBooks = accountBookRepository.findAllByUserIdAndType(userId, CategoryType.INCOME);
+
+        if (accountBooks.isEmpty()) {
+            throw new AccountBookErrorException(AccountBookErrorCode.NOT_FOUND_INCOME);
+        }
 
         return accountBooks.stream()
                 .map(AccountBookIncomeResponse::from)
