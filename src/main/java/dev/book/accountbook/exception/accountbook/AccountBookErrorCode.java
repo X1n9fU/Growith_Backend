425c67ba0e49dab29d5ab1bea3f0d40500230be5
BHC-Chicken <tij05078@gmail.com>
2025-04-18 14:40:47 +0900

    [feat] 예산 찾지 못했을 경우 예외 추가

--- src/main/java/dev/book/accountbook/exception/accountbook/AccountBookErrorCode.java
+++ src/main/java/dev/book/accountbook/exception/accountbook/AccountBookErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum AccountBookErrorCode implements ErrorCode {
     NOT_FOUND_SPEND(HttpStatus.NOT_FOUND, "존재하지 않는 소비내역입니다."),
     NOT_FOUND_INCOME(HttpStatus.NOT_FOUND, "존재하지 않는 수입내역입니다."),
-    NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND,"존재하지 않는 카테고리입니다.");
+    NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND,"존재하지 않는 카테고리입니다."),
+    NOT_FOUND_BUDGET(HttpStatus.NOT_FOUND, "존재하지 않는 예산입니다.");
 
     private final HttpStatus status;
     private final String message;
