10a209887bd09c4c1a960464dd9bd70d9e048d25
X1n9fU <minkyeong0244@gmail.com>
2025-04-21 18:03:47 +0900

    Revert "[feature] Achievement 기능 추가 #40"
    This reverts commit 34d709f8f2efdc402ce9ddea422bcd7ce9851491.

--- src/main/java/dev/book/achievement/repository/AchievementRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.book.achievement.repository;
-
-import dev.book.achievement.entity.Achievement;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AchievementRepository extends JpaRepository<Achievement, Long> {
-}
