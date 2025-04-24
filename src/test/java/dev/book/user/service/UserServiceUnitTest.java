bbf8afbe58dcb17e2252bec25d20dbe1d9df387a
X1n9fU <minkyeong0244@gmail.com>
2025-04-25 02:41:48 +0900

    [test] User, UserFriend 단위 테스트 수정 및 추가 #40

--- src/test/java/dev/book/user/service/UserServiceUnitTest.java
+++ src/test/java/dev/book/user/service/UserServiceUnitTest.java
@@ -1,9 +1,19 @@
 package dev.book.user.service;
 
+import dev.book.achievement.achievement_user.entity.AchievementUser;
+import dev.book.achievement.achievement_user.repository.AchievementUserRepository;
+import dev.book.achievement.entity.Achievement;
 import dev.book.global.config.security.dto.CustomUserDetails;
 import dev.book.global.config.security.jwt.JwtAuthenticationToken;
 import dev.book.global.config.security.jwt.JwtUtil;
+import dev.book.global.entity.Category;
+import dev.book.global.exception.category.CategoryException;
+import dev.book.global.repository.CategoryRepository;
+import dev.book.user.dto.request.UserCategoriesRequest;
 import dev.book.user.dto.request.UserProfileUpdateRequest;
+import dev.book.user.dto.response.UserAchievementResponse;
+import dev.book.user.dto.response.UserCategoryResponse;
+import dev.book.user.dto.response.UserChallengeInfoResponse;
 import dev.book.user.dto.response.UserProfileResponse;
 import dev.book.user.entity.UserEntity;
 import dev.book.user.exception.UserErrorCode;
@@ -21,6 +31,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
@@ -35,6 +48,12 @@ class UserServiceUnitTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private AchievementUserRepository achievementUserRepository;
 
     @Mock
     private HttpServletRequest request;
@@ -131,4 +150,100 @@ class UserServiceUnitTest {
         assertThatThrownBy(() -> userService.deleteUser(request, response, null))
                 .isInstanceOf(NullPointerException.class);
     }
+
+    @Test
+    @DisplayName("유저의 카테고리를 변경한다.")
+    void changeCategories(){
+        //given
+        given(userRepository.findByEmail(any())).willReturn(Optional.ofNullable(userDetails.user()));
+        List<String> categories = List.of("food", "cafe_snack");
+        List<Category> categoryList = List.of(new Category("food", "음식"), new Category("cafe_snack", "카페 / 간식") );
+        given(categoryRepository.findByCategoryIn(categories)).willReturn(categoryList);
+        UserCategoriesRequest categoriesRequest = new UserCategoriesRequest(categories);
+
+        //when
+        userService.updateUserCategories(categoriesRequest, userDetails);
+
+        //then
+        assertThat(userDetails.user().getUserCategory().size()).isEqualTo(2);
+
+    }
+
+    @Test
+    @DisplayName("카테고리 내역이 없다면 에러를 반환한다.")
+    void checkCategories(){
+        //given
+        given(userRepository.findByEmail(any())).willReturn(Optional.ofNullable(userDetails.user()));
+        List<String> categories = List.of("food", "cafe", "snack");
+        List<Category> categoryList = List.of(new Category("food", "음식"));
+        given(categoryRepository.findByCategoryIn(categories)).willReturn(categoryList);
+        UserCategoriesRequest categoriesRequest = new UserCategoriesRequest(categories);
+
+        //when & then
+        assertThatThrownBy(() -> userService.updateUserCategories(categoriesRequest, userDetails))
+                .isInstanceOf(CategoryException.class)
+                        .hasMessageContaining("일치하지 않는 카테고리가 존재합니다.");
+
+    }
+
+    @Test
+    @DisplayName("유저의 카테고리를 반환한다.")
+    void getUserCategories(){
+        //given
+        UserEntity user = UserBuilder.withCategory(); // food, cafe_snack 으로 초기화
+        userDetails = new CustomUserDetails(user);
+        SecurityContextHolder.getContext().setAuthentication(new JwtAuthenticationToken(userDetails, userDetails.getAuthorities()));
+
+        given(userRepository.findByEmailWithCategories(any())).willReturn(Optional.ofNullable(userDetails.user()));
+
+        //when
+        UserCategoryResponse userCategoryResponse = userService.getUserCategories(userDetails);
+
+        //then
+        assertThat(userCategoryResponse.categories().size()).isEqualTo(2);
+        assertThat(userCategoryResponse.categories().contains("음식")).isTrue();
+        assertThat(userCategoryResponse.categories().contains("카페 / 간식")).isTrue();
+    }
+
+    @Test
+    @DisplayName("유저의 업적들을 반환한다.")
+    void getUserAchievement(){
+        //given
+        UserEntity user = userDetails.user();
+        Achievement achievement = new Achievement("업적", "내용");
+        AchievementUser achievementUser = new AchievementUser(user, achievement);
+        given(achievementUserRepository.findAllByUser(user)).willReturn(List.of(achievementUser));
+
+        //when
+        List<UserAchievementResponse> userAchievementResponses = userService.getUserAchievement(userDetails);
+
+        //then
+        assertThat(userAchievementResponses.size()).isEqualTo(1);
+        assertThat(userAchievementResponses.get(0).title()).isEqualTo(achievement.getTitle());
+        assertThat(userAchievementResponses.get(0).content()).isEqualTo(achievement.getContent());
+    }
+
+    @Test
+    @DisplayName("유저의 챌린지 내용을 반환한다.")
+    void getUserChallenge(){
+        //given
+        UserEntity user = userDetails.user();
+        user.plusSavings(10000L);
+        user.plusFinishedChallenge();
+        user.plusCompleteChallenge();
+        user.plusParticipatingChallenge();
+
+        //when
+        UserChallengeInfoResponse userChallengeInfoResponse = userService.getUserChallengeInfo(userDetails);
+
+        //then
+        assertThat(userChallengeInfoResponse.savings()).isEqualTo(user.getSavings());
+        assertThat(userChallengeInfoResponse.finishedChallenge()).isEqualTo(user.getFinishedChallenge());
+        assertThat(userChallengeInfoResponse.completedChallenges()).isEqualTo(user.getCompletedChallenges());
+        assertThat(userChallengeInfoResponse.participatingChallenges()).isEqualTo(user.getParticipatingChallenges());
+
+    }
+
+
+
 }
