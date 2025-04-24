d025112e9623e15f1ac783f864e78af48ab0f82e
X1n9fU <minkyeong0244@gmail.com>
2025-04-25 04:17:07 +0900

    [refactor] UserFriendErrorCode 절차 수정 #40

--- src/main/java/dev/book/user/user_friend/exception/UserFriendErrorCode.java
+++ src/main/java/dev/book/user/user_friend/exception/UserFriendErrorCode.java
@@ -12,7 +12,7 @@ public enum UserFriendErrorCode implements ErrorCode {
     FRIEND_NOT_FOUND(HttpStatus.NOT_FOUND, "친구 유저를 찾을 수 없습니다."),
     INVITING_NOT_FOUND(HttpStatus.NOT_FOUND, "초대 토큰과 알맞는 내역을 찾을 수 없습니다."),
     DUPLICATE_INVITATION(HttpStatus.CONFLICT, "이미 존재하는 초대 요청입니다."),
-    ALREADY_MAKE_INVITATION(HttpStatus.BAD_REQUEST, "이미 초대 요청이 만들어진 상태입니다"),
+    ALREADY_MADE_INVITATION(HttpStatus.BAD_REQUEST, "이미 초대 요청이 만들어진 상태입니다"),
     MYSELF_INVITATION(HttpStatus.BAD_REQUEST, "내가 나에게 초대 요청을 보낼 수 없습니다."),
     FRIEND_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "친구의 요청을 찾을 수 없습니다.");
 
